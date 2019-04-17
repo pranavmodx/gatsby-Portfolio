ed61abee8dfd0045c28f9cae940057715f427c63
pranavmodx <pranavmodx@gmail.com>
2019-04-17 10:58:22 +0530

    initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,10 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
-                    </ul>
-                    <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                        <li><a href="https://twitter.com/pranavmodx" className="icon fa-twitter" target="_blank"><span className="label">Twitter</span></a></li>
+                        <li><a href="https://github.com/pranavmodx" className="icon fa-github" target="_blank"><span className="label">Github</span></a></li>
+                        <li><a href="mailto:pranavmodx@gmail.com" className="icon fa-envelope-o" target="_blank"><span className="label">Email</span></a></li>
+                        <li><a href="https://medium.freecodecamp.org/@pranavmodx" className="icon fa-medium" target="_blank"><span className="label">Medium</span></a></li>
                     </ul>
                 </div>
             </div>
